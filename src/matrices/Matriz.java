80c9694cdb91f300bac64f51437b71b99d7329a3
zeyka <dark_st0rm@hotmail.com>
2015-01-20 20:31:54 +0100

    add matriz inversa

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -16,7 +16,7 @@ import java.util.Random;
 public class Matriz {
     private int[][]datos;
     private Random rnd = new Random();
-    
+
     public Matriz(int filas, int columnas, boolean inicializarAleatorio){
         datos = new int[columnas][];
         for(int i=0; i<columnas; i++){
@@ -29,40 +29,65 @@ public class Matriz {
     public Matriz(Dimension d, boolean inicializarAleatorio){
         this(d.height, d.width, inicializarAleatorio);
     }
-    
+
     public Dimension getDimension(){
         return new Dimension(datos.length, datos[0].length);
     }
-    
-    public static Matriz sumarDosMatrices(Matriz a, Matriz b) throws DimensionesIncompatibles { 
-        if(! a.getDimension().equals(b.getDimension())) throw new DimensionesIncompatibles("La suma de matrices requiere matrices de las mismas dimensiones");        
-        int i, j, filasA, columnasA; 
-        filasA = a.getDimension().height; 
-        columnasA = a.getDimension().width; 
+
+    public static Matriz sumarDosMatrices(Matriz a, Matriz b) throws DimensionesIncompatibles {
+        if(! a.getDimension().equals(b.getDimension())) throw new DimensionesIncompatibles("La suma de matrices requiere matrices de las mismas dimensiones");
+        int i, j, filasA, columnasA;
+        filasA = a.getDimension().height;
+        columnasA = a.getDimension().width;
         Matriz matrizResultante = new Matriz(filasA, columnasA, false);
-        for (j = 0; j < filasA; j++) { 
-            for (i = 0; i < columnasA; i++) { 
-                matrizResultante.datos[i][j] += a.datos[i][j] + b.datos[i][j]; 
-            } 
-        } 
-        return matrizResultante; 
-    } 
-    
+        for (j = 0; j < filasA; j++) {
+            for (i = 0; i < columnasA; i++) {
+                matrizResultante.datos[i][j] += a.datos[i][j] + b.datos[i][j];
+            }
+        }
+        return matrizResultante;
+    }
+
     @Override
     public String toString(){
         String ret = "";
         ret += "[\n";
         for (int i = 0; i < getDimension().width; i++) {
             ret += "(";
-            for (int j = 0; j < getDimension().height; j++) {  
-                ret += String.format("%3d", datos[i][j]); 
+            for (int j = 0; j < getDimension().height; j++) {
+                ret += String.format("%3d", datos[i][j]);
                 if (j != getDimension().height - 1) ret += ", ";
-            } 
+            }
             ret += ")";
             if (i != getDimension().width - 1) ret += ",";
             ret += "\n";
-        } 
+        }
         ret += "]\n";
         return ret;
     }
+
+    //matriz inversa
+	    public static Matriz inversa(Matriz d) throws CloneNotSupportedException {
+	        int n=d.getDimension().height;  //dimensión de la matriz
+	        Matriz a=(Matriz)d.clone();
+	        Matriz b=new Matriz(n,n,false);
+	        Matriz c=new Matriz(n,n,false);
+
+	        for(int i=0; i<n; i++){
+	            b.datos[i][i]=1;
+	        }
+
+	        for(int k=0; k<n-1; k++){
+	            for(int i=k+1; i<n; i++){
+	                for(int s=0; s<n; s++){
+	                    b.datos[i][s]-=a.datos[i][k]*b.datos[k][s]/a.datos[k][k];
+	                }
+	                for(int j=k+1; j<n; j++){
+	                    a.datos[i][j]-=a.datos[i][k]*a.datos[k][j]/a.datos[k][k];
+	                }
+	            }
+	        }
+	        return a;
+	    }
+
 }
